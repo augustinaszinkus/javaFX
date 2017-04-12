2a54ae775304bb9d038bcbc52528f2fee29ffa67
augustinaszinkus <azauge@gmail.com>
2017-04-12 19:55:16 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label text;
+
+    public void sayHi (ActionEvent event) {
+        String prefix = text.getText();
+        text.setText(prefix + " Augustinas");
+    }
+
+}
